21851129c44edd44bd7ec8db2b343a69aa5a3d62
xuchao <your email>
2019-08-02 15:01:59 +0800

    test antd table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Table } from 'antd';
 import './App.css';
 
 function App() {
+	const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    width: 150,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    width: 150,
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+  },
+];
+
+const data = [];
+for (let i = 0; i < 100; i++) {
+  data.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  });
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table columns={columns} dataSource={data} pagination={false} scroll={{ y: 240 }} />
     </div>
   );
 }
